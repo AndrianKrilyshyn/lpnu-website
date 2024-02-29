c5cbea9cfdb0b5cb905c7a7c860d9655a9927b71
AndrianKrilyshyn <148096063+AndrianKrilyshyn@users.noreply.github.com>
2024-02-29 13:49:52 +0000

    Add lr-4 and decoration to all projects

--- js/LR-4_script.js
+++ js/LR-4_script.js
@@ -31,36 +31,95 @@ btnArray.addEventListener("click", getArray);
 function getArray() {
   const field = document.getElementById("array").value;
   const array = field.split(",");
-  const avgElement = array[Math.floor(array.length*0.5)];
-  if(isNumber(parseInt(avgElement))){
+  const avgElement = array[Math.floor(array.length * 0.5)];
+  if (isNumber(parseInt(avgElement))) {
     let number = parseInt(avgElement);
-    let sum=0;
-    let k =10;
-    while(number>0){
-        sum += number%k;
-        number = Math.floor(number/k);
-        k*=10;
+    let sum = 0;
+    let k = 10;
+    while (number > 0) {
+      sum += number % k;
+      number = Math.floor(number / k);
+      k *= 10;
     }
     alert(sum);
-  }else{
+  } else {
     alert(avgElement);
   }
 }
 function isNumber(value) {
-    return typeof value === 'number' && !isNaN(value);
-  }
+  return typeof value === "number" && !isNaN(value);
+}
 
-  // Завдання 18
+// Завдання 18
 const btnMoreTen = document.getElementById("btn-more-ten");
 btnMoreTen.addEventListener("click", getArrayWithMoreTenElem);
 
-function getArrayWithMoreTenElem(){
-    const array = document.getElementById("moreTen").value.split(",");
-    const newArray=[];
-    for(let i=0; i<array.length;i++){
-        if(parseInt(array[i])>10){
-            newArray.push(parseInt(array[i]));
-        }
+function getArrayWithMoreTenElem() {
+  const array = document.getElementById("moreTen").value.split(",");
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (parseInt(array[i]) > 10) {
+      newArray.push(parseInt(array[i]));
     }
-     newArray.length>0?alert(newArray):alert("Таких елементів немає");
+  }
+  newArray.length > 0 ? alert(newArray) : alert("Таких елементів немає");
 }
+
+// Завдання 21
+
+const btnPairs = document.getElementById("btn-pairs");
+btnPairs.addEventListener("click", createTable);
+
+function createTable() {
+  const input = document.getElementById("parisArray").value.trim();
+  const pairs = input.split(",").map(pair => pair.trim());
+
+  if (pairs.length % 2 !== 0) {
+    alert("Please enter an even number of values.");
+    return;
+  }
+
+  const table = document.getElementById("outputTable");
+
+  for (let i = 0; i < pairs.length; i++) {
+    const pairNumb = pairs[i].split(" ");
+    const rowIndex = parseInt(pairNumb[0]);
+    const colIndex = parseInt(pairNumb[1]);
+
+    if (!table.rows[parseInt(pairNumb[0])]) {
+      let k = 1 + rowIndex;
+      while (k > 0) {
+        table.insertRow();
+        k--;
+      }
+    }
+
+    const row = table.rows[rowIndex];
+
+    if (!row.cells[colIndex]) {
+      let k = 1 + colIndex;
+      while (k > 0) {
+        row.insertCell();
+        k--;
+      }
+    }
+
+    const cell = row.cells[colIndex];
+    cell.textContent = i;
+  }
+}
+
+// Завдання 26
+const btnGetArrayElem = document.getElementById("btn-get-array-elem");
+btnGetArrayElem.addEventListener("click", getArrayElem);
+function getArrayElem(){
+  const elements = document.getElementById("getArrayElem").value.split(",").map(elem => elem.trim());
+  const sizeArray = document.getElementById("getArraySize").value;
+  const outputResult = document.getElementById("newArray");
+  outputResult.innerText = "[ "+getFirst(elements,sizeArray).join(", ")+" ]";
+
+}
+function getFirst(array, n){
+  return n>0? array.slice(0, n): array.slice(0, array.length-n);
+}
+
