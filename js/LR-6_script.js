19985a8c07dbc709a6c9cdf8c21221fb00937ba1
AndrianKrilyshyn <148096063+AndrianKrilyshyn@users.noreply.github.com>
2024-03-02 10:05:55 +0000

    Add lr-6

---
+++ js/LR-6_script.js
@@ -0,0 +1,24 @@
+// Завдання 1.3
+const input = document.getElementById("letters");
+const output = document.getElementById("key-result");
+input.addEventListener("keypress", (code) => {
+  output.innerText = "Key pressed: "+String.fromCharCode(code.charCode)+"\nCode: "+code.charCode;
+});
+
+// Завдання 2.15
+const img = document.getElementById("img-r2d2");
+img.addEventListener("mouseover", changeToC3PO);
+img.addEventListener("mouseout", changeToR2D2);
+img.addEventListener("click", getPath);
+
+function changeToC3PO() {
+    img.src = "../img/c-3PO.png";
+}
+
+function changeToR2D2() {
+    img.src = "../img/r2d2.jpeg";
+}
+
+function getPath(){
+    alert("Шлях до файлу малюнка: "+img.src);
+}
