345c1d504168fb002df5c173e2419d28a8a4c33d
AndrianKrilyshyn <148096063+AndrianKrilyshyn@users.noreply.github.com>
2024-03-25 08:04:27 +0000

    lr 1-3 complete

--- js/LR-3_Task4.js
+++ js/LR-3_Task4.js
@@ -3,8 +3,12 @@ const closeBtn = document.getElementById("close");
 const urlPage = "https://lpnu.ua/";
 
 const windowFeatures = "left=100,top=100,width=600,height=200,location=yes,toolbar=yes,scrollbars=yes";
+
+const height = screen.height;
+const width = screen.width;
+let windowFeaturesNew = "width=" + width + "height=" + height +"fullscreen=yes";
 let newWindow = null;
-openBtn.addEventListener("click", openWindow);
+openBtn.addEventListener("click", openSiteWindow);
 closeBtn.addEventListener("click", closeWindow);
 function openWindow() {
   newWindow = window.open(urlPage, "_blank", windowFeatures);
@@ -17,3 +21,15 @@ function closeWindow() {
     alert("Вікно не відкрито");
   }
 }
+function openSiteWindow(){
+  if(checkWidth()){
+    windowFeaturesNew = "width=" + width + ",height=" + height +"fullscreen=yes";
+    newWindow = window.open(urlPage, "_blank", windowFeaturesNew);
+  }else{
+    newWindow = window.open(urlPage, "_blank");
+  }
+}
+function checkWidth(){
+  return window.innerWidth>700;
+}
+
